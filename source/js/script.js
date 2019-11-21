868dea2682ff5480499eb4e3c200543eb41f5fb2
Langenhorn77 <ch_ur@mail.ru>
2019-11-21 04:21:06 +0300

    5 commit

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,27 @@
 const requestButton = document.querySelector("#request-button");
-const popupWindow = document.querySelector(".popup");
+
 const overlay = document.querySelector(".overlay");
 
-const nameInput = popupWindow.querySelector("#your-name");
-const phoneInput = popupWindow.querySelector("#your-phone");
-const messageInput = popupWindow.querySelector("#your-message");
-const closeButton = popupWindow.querySelector(".popup__close");
+
+// Элементы всплывающего окна
+const popupWindow = document.querySelector(".popup--main");
+
+const popupForm = popupWindow.querySelector("#popup__form");
+const popupNameInput = popupWindow.querySelector("#your-name");
+const popupPhoneInput = popupWindow.querySelector("#your-phone");
+const popupMessageInput = popupWindow.querySelector("#your-message");
+const popupCloseButton = popupWindow.querySelector(".popup__close");
+const popupErrorNameMessage = popupWindow.querySelector(".popup__error-message--name");
+const popupErrorPhoneMessage = popupWindow.querySelector(".popup__error-message--phone");
+
+
+// Элементы формы в подвале
+const footerForm = document.querySelector(".feedback__form");
+const footerFormNameInput = footerForm.querySelector("#name");
+const footerFormPhoneInput = footerForm.querySelector("#phone");
+const footerFormErrorNameMessage = footerForm.querySelector(".feedback__error-message--name");
+const footerFormErrorPhoneMessage = footerForm.querySelector(".feedback__error-message--phone");
+
 const ESC_KEYCODE = 27;
 
 const navCoverButton = document.querySelector(".navigation__button");
@@ -18,7 +34,7 @@ const width = window.innerWidth || document.documentElement.clientWidth ||
   document.body.clientWidth;
 
 const promoButton = document.querySelector(".promo__button");
-const formBlock = document.querySelector(".feedback__form");
+
 
 const scrollLink = document.querySelector(".promo__link");
 const advantagesBlock = document.querySelector(".advantages__list");
@@ -32,7 +48,7 @@ function handleButtonClick(element) {
 // Навешивание обработчиков события (клик + скролл)
 promoButton.addEventListener('click', function (e) {
   e.preventDefault();
-  handleButtonClick(formBlock)
+  handleButtonClick(footerForm)
 });
 
 scrollLink.addEventListener('click', function (e) {
@@ -44,6 +60,7 @@ scrollLink.addEventListener('click', function (e) {
 
 function toggleMenu(elem, btn) {
   elem.classList.toggle('menu-closed');
+  elem.classList.toggle('menu-opened');
   btn.classList.toggle('button--closed');
 }
 
@@ -51,7 +68,6 @@ function toggleMenu(elem, btn) {
 if (width < 768) {
   navBlock.addEventListener("click", function () {
     toggleMenu(navBlock, navCoverButton);
-    toggleMenu(contactsBlock, contactsCoverButton);
   });
   navBlock.classList.add("menu-closed");
   navCoverButton.classList.add("button--closed");
@@ -60,21 +76,24 @@ if (width < 768) {
 if (width < 768) {
   contactsBlock.addEventListener("click", function () {
     toggleMenu(contactsBlock, contactsCoverButton);
-    toggleMenu(navBlock, navCoverButton);
   });
+  contactsBlock.classList.add("menu-closed");
+  contactsCoverButton.classList.add("button--closed");
 }
 
 // Открытие всплывающего окна и навешивание обработчиков события для его закрытия
 function openPopup() {
   if (popupWindow) {
     popupWindow.classList.remove("popup--closed");
+    document.body.style.overflow = 'hidden';
   }
   if (overlay) {
     overlay.classList.remove("overlay--closed");
+
     overlay.addEventListener("click", overlayClickHandler);
   }
-  if (nameInput) {
-    nameInput.focus()
+  if (popupNameInput) {
+    popupNameInput.focus()
   }
   document.addEventListener("keydown", popupEscPressHandler);
 }
@@ -86,6 +105,7 @@ function closePopup() {
   overlay.classList.add("overlay--closed");
   overlay.removeEventListener("click", overlayClickHandler);
   document.removeEventListener("keydown", popupEscPressHandler);
+  document.body.style.overflow = 'visible';
 }
 
 // Навешивание обработчиков события для вызова всплывающего окна и его закрытия
@@ -93,8 +113,8 @@ if (requestButton) {
   requestButton.addEventListener("click", openPopup);
 }
 
-if (closeButton) {
-  closeButton.addEventListener("click", closePopup);
+if (popupCloseButton) {
+  popupCloseButton.addEventListener("click", closePopup);
 }
 
 //Функции закрытия всплывающего окна по нажатию ESC или клика вне окна
@@ -109,32 +129,278 @@ function overlayClickHandler() {
   closePopup();
 }
 
-//Сохранение значений полей input в localStorage
-
-if (nameInput) {
-  nameInput.value = localStorage.getItem('name');
-  nameInput.oninput = () => {
-    localStorage.setItem('name', nameInput.value)
-  };
-}
-
-if (phoneInput) {
-  phoneInput.value = localStorage.getItem('phone');
-  phoneInput.oninput = () => {
-    localStorage.setItem('phone', phoneInput.value)
-  };
-}
-
-if (messageInput) {
-  messageInput.value = localStorage.getItem('message');
-  messageInput.oninput = () => {
-    localStorage.setItem('message', messageInput.value)
-  };
-}
 
 //Создание маски для input
 
 const maskOptions = {
-  mask: '+{7}(000)000-00-00'
+  mask: '{8}0000000000'
 };
-const mask = IMask(phoneInput, maskOptions);
+const popupMask = IMask(popupPhoneInput, maskOptions);
+const footerMask = IMask(footerFormPhoneInput, maskOptions);
+
+
+// Создание окна результата загрузки формы
+function createResultPopup(result) {
+  let fragment = document.createDocumentFragment();
+  let resultTemplate = document.querySelector('#' + result)
+    .content
+    .querySelector('.popup--' + result);
+  let resultItem = resultTemplate.cloneNode(true);
+  fragment.appendChild(resultItem);
+
+  popupWindow.parentNode.insertBefore(fragment, popupWindow);
+}
+
+// Успех загрузки
+function sendFormSuccessHandler() {
+  createResultPopup('success');
+
+  const successWindow = document.querySelector('.popup--success');
+  const successButton = successWindow.querySelector('.popup__button--success');
+
+  function closeSuccessWindow() {
+    successWindow.remove();
+  }
+
+  document.addEventListener('click', closeSuccessWindow);
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeSuccessWindow();
+    }
+  });
+  successButton.addEventListener('click', closeSuccessWindow);
+}
+
+// Ошибка загрузки
+function sendFormErrorHandler() {
+  createResultPopup('error');
+
+  const errorWindow = document.querySelector('.popup--error');
+  const errorButton = errorWindow.querySelector('.popup__button--error');
+
+  function closeErrorWindow() {
+    errorWindow.remove();
+  }
+
+  document.addEventListener('click', closeErrorWindow);
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeErrorWindow();
+    }
+  });
+  errorButton.addEventListener('click', closeErrorWindow);
+}
+
+// Отправка данных на сервер
+
+function postData(data) {
+  fetch('https://echo.htmlacademy.ru', {
+    method: 'POST',
+    body: data
+  }).then(
+    function (response) {
+      if (response.status === 200) {
+        return Promise.resolve(response)
+      } else {
+        return Promise.reject(new Error(response.statusText))
+      }
+    }
+  ).then(function () {
+    closePopup();
+    sendFormSuccessHandler();
+  }).catch(function () {
+    closePopup();
+    sendFormErrorHandler();
+  });
+}
+
+// Правила валидации
+
+const nameInputRules = {
+  length: [
+    10,
+    'Длина имени должна быть не более 10 символов!'
+  ],
+  pattern: [
+    /^[a-zа-яё]+$/i,
+    'Имя должно состоять только из букв!'
+  ]
+};
+
+const phoneInputRules = {
+  length: [
+    11,
+    'Номер телефона должен быть 11-значным!'
+  ],
+  pattern: [
+    /^[0-9]+$/,
+    'Номер телефона должен состоять только из цифр!'
+  ]
+};
+
+
+function CustomValidation() {
+}
+
+CustomValidation.prototype = {
+  nameInvalidities: [],
+  phoneInvalidities: [],
+  nameFooterInvalidities: [],
+  phoneFooterInvalidities: [],
+
+  // Валидация поля Имя
+
+  checkNameValidity: function (input) {
+    let result = true;
+    let inputFieldValues = input.value.trim().split(/\s+/);
+    if (!(inputFieldValues[0] === '')) {
+      if (input.value.length > nameInputRules.length[0]) {
+        this.addInvalidity(this.nameInvalidities, nameInputRules.length[1]);
+        result = false
+      }
+
+      for (let i = 0; i < inputFieldValues.length; i++) {
+        if (!inputFieldValues[i].match(nameInputRules.pattern[0])) {
+          this.addInvalidity(this.nameInvalidities, nameInputRules.pattern[1]);
+          result = false
+        }
+      }
+    }
+    return result;
+  },
+
+  // Валидация поля Телефон
+
+  checkPhoneValidity: function (input) {
+    let result = true;
+    let inputFieldValues = input.value;
+    if (!(inputFieldValues[0] === '')) {
+      if (inputFieldValues.length !== phoneInputRules.length[0]) {
+        this.addInvalidity(this.phoneInvalidities, phoneInputRules.length[1]);
+        result = false
+      }
+
+      for (let i = 0; i < inputFieldValues.length; i++) {
+        if (!inputFieldValues[i].match(phoneInputRules.pattern[0])) {
+          this.addInvalidity(this.phoneInvalidities, phoneInputRules.pattern[1]);
+          result = false
+        }
+      }
+    }
+    return result;
+  },
+
+  // Добавление сообщения об ошибке в массив ошибок
+  addInvalidity: function (array, message) {
+    array.push(message);
+  },
+
+  getInvaliditiesForHTML: function (array) {
+    return array.join('<br>');
+  }
+};
+
+// Обработчик ввода в поля input на всплывающем окне
+
+function validationInputHandler(evt, element, classN) {
+  if (!(evt.target.value === '')) {
+    evt.target.setCustomValidity('');
+    let errorMessage = element.querySelector(classN);
+    if (errorMessage) {
+      errorMessage.innerHTML = '';
+    }
+  }
+}
+
+// Обработчик отправки формы
+
+function sendData(evt, form, name, phone, nameMessage, phoneMessage) {
+  let sendingData = new FormData(form);
+  let inputCustomValidation = new CustomValidation(); // Создадим объект CustomValidation
+  CustomValidation.prototype.nameInvalidities = [];
+  CustomValidation.prototype.phoneInvalidities = [];
+  let nameIsIncorrect = !inputCustomValidation.checkNameValidity(name);
+  let phoneIsIncorrect = !inputCustomValidation.checkPhoneValidity(phone);
+  if (nameIsIncorrect) {
+    nameMessage.innerHTML = inputCustomValidation.getInvaliditiesForHTML(CustomValidation.prototype.nameInvalidities);
+    name.style.borderColor = 'red';
+    evt.preventDefault();
+  }
+  if (phoneIsIncorrect) {
+    phoneMessage.innerHTML = inputCustomValidation.getInvaliditiesForHTML(CustomValidation.prototype.phoneInvalidities);
+    phone.classList.add('input-error');
+    phone.style.borderColor = 'red';
+    evt.preventDefault();
+  }
+  if (!nameIsIncorrect && !phoneIsIncorrect) {
+    evt.preventDefault();
+    postData(sendingData);
+  }
+}
+
+//    ОБРАБОТКА ФОРМЫ В ПОДВАЛЕ
+
+// Навешивание обработчиков ввода в поля input в форме в подвале
+
+footerFormNameInput.addEventListener('input', function (evt) {
+  validationInputHandler(evt, footerForm, ".feedback__error-message--name");
+  footerFormNameInput.style.borderColor = 'transparent';
+  CustomValidation.prototype.nameInvalidities = [];
+});
+footerFormPhoneInput.addEventListener('input', function (evt) {
+  validationInputHandler(evt, footerForm, ".feedback__error-message--phone");
+  footerFormPhoneInput.style.borderColor = 'transparent';
+  CustomValidation.prototype.phoneInvalidities = [];
+});
+
+// Обработка отправки формы в подвале
+
+footerForm.addEventListener('submit', function (evt) {
+  sendData(evt, footerForm, footerFormNameInput, footerFormPhoneInput, footerFormErrorNameMessage, footerFormErrorPhoneMessage);
+});
+
+
+//    ОБРАБОТКА ФОРМЫ ВСПЛЫВАЮЩЕГО ОКНА
+
+//Сохранение значений полей input в localStorage
+
+if (popupNameInput) {
+  popupNameInput.value = localStorage.getItem('name');
+  popupNameInput.oninput = () => {
+    localStorage.setItem('name', popupNameInput.value);
+  };
+}
+
+if (popupPhoneInput) {
+  popupPhoneInput.value = localStorage.getItem('phone');
+  popupPhoneInput.oninput = () => {
+    localStorage.setItem('phone', popupPhoneInput.value)
+  };
+}
+
+if (popupMessageInput) {
+  popupMessageInput.value = localStorage.getItem('message');
+  popupMessageInput.oninput = () => {
+    localStorage.setItem('message', popupMessageInput.value)
+  };
+}
+
+
+// Навешивание обработчиков ввода в поля input на всплывающем окне
+
+popupNameInput.addEventListener('input', function (evt) {
+  validationInputHandler(evt, popupWindow, ".popup__error-message--name");
+  popupNameInput.style.borderColor = 'transparent';
+  CustomValidation.prototype.nameInvalidities = [];
+});
+popupPhoneInput.addEventListener('input', function (evt) {
+  validationInputHandler(evt, popupWindow, ".popup__error-message--phone");
+  popupPhoneInput.style.borderColor = 'transparent';
+  CustomValidation.prototype.phoneInvalidities = [];
+});
+
+// Обработка отправки формы всплывающего окна
+
+popupForm.addEventListener('submit', function (evt) {
+  sendData(evt, popupForm, popupNameInput, popupPhoneInput, popupErrorNameMessage, popupErrorPhoneMessage);
+});
